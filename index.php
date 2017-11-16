7666aa51cddfd3780ec33873b9af0dfd9744b392
Urbán Laura <urbanlaura@Urban-Air.localdomain>
2017-11-16 14:08:02 +0100

    start

---
+++ index.php
@@ -0,0 +1,12 @@
+<head>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="js/ajax.js"></script>
+  <link rel='stylesheet' type='text/css' href='css/style.css'> 
+</head>
+
+<div id="calc_auto_regado"></div>
+<div id="calc_motor_regado"></div>
+<div id="calc_atiras"></div>
+<div id="calc_eredet"></div>
+<div id="calc_teljesitmeny"></div>
+<div id="calc_cegauto_ado"></div>
---
+++ js/ajax.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+    $('#calc_auto_regado').html('<object data="https://www.ecdh.hu/kalkulatorok/calc_auto_regado/?css=ecdh" style="height:527px;"/>');
+   // document.querySelector('#calc_auto_regado').setAttribute("src", "https://www.ecdh.hu/kalkulatorok/calc_auto_regado/?css=ecdh");
+    $('#calc_motor_regado').html('<object data="https://www.ecdh.hu/kalkulatorok/calc_motor_regado/?css=ecdh" style="height:416.45px;"/>');
+    $('#calc_atiras').html('<object data="https://www.ecdh.hu/kalkulatorok/calc_atiras/?css=ecdh" style="height:564px;"/>');
+    $('#calc_eredet').html('<object data="https://www.ecdh.hu/kalkulatorok/calc_eredet/?css=ecdh" style="height:311px;"/>');
+    $('#calc_teljesitmeny').html('<object data="https://www.ecdh.hu/kalkulatorok/calc_teljesitmeny/?css=ecdh" style="height:369px;"/>');
+    $('#calc_cegauto_ado').html('<object data="https://www.ecdh.hu/kalkulatorok/calc_cegauto_ado/?css=ecdh" style="height:356px;"/>');
+})
